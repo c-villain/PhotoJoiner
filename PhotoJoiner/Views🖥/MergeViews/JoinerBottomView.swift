e7e41fc568fed46ebcf0eeeff95ac1f3e6795b6a
Alexander Kraev <lexkraev@gmail.com>
2020-08-12 23:26:53 +0300

    add one joiner view instead 3 views

---
+++ PhotoJoiner/Views🖥/MergeViews/JoinerBottomView.swift
@@ -0,0 +1,82 @@
+//
+//  JoinerBottomView.swift
+//  PhotoJoiner
+//
+//  Created by Alexander Kraev on 10.08.2020.
+//  Copyright © 2020 Alexander Kraev. All rights reserved.
+//
+
+import SwiftUI
+
+struct JoinerBottomView: View {
+    
+    @ObservedObject var viewModel: ImagesViewModel
+    
+//    @Binding var showingTopPopup: Bool
+//    var showingTopPopup: Binding<Bool>
+    
+    private let shuffleTappedCallback: (Bool) -> ()
+    private let savingTappedCallback: () -> ()
+    
+    public init(viewModel: ImagesViewModel,
+                 shuffleTapped: @escaping (Bool) -> (),
+                 savingTapped: @escaping () -> () ){
+//                 showingTopPopup: Binding<Bool>){
+        self.viewModel = viewModel
+        self.shuffleTappedCallback = shuffleTapped
+        self.savingTappedCallback = savingTapped
+//        self.showingTopPopup = showingTopPopup
+    }
+    
+    func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        print("Save finished!")
+    }
+    
+    var body: some View {
+        HStack{
+                        
+            Button(action: {
+                withAnimation{
+                    DispatchQueue.main.async {
+                        self.shuffleTappedCallback(false)
+                        self.viewModel.shuffle()
+                        self.shuffleTappedCallback(true)
+                    }
+                }
+            }) {
+                HStack(spacing: 6){
+                    Text("Shuffle")
+                    Image("Shuffle")
+                        .renderingMode(.template)
+                        .foregroundColor(.white)
+                }.foregroundColor(.white)
+                .padding()
+            }
+            .background(Color("Color"))
+            .cornerRadius(8)
+            
+            Spacer()
+            Button(action: {
+                self.viewModel.saveMergedImage()
+                savingTappedCallback()
+//                self.showingTopPopup = true
+            }) {
+                HStack(spacing: 6){
+                    Text("Join photos")
+                    Image("arrow").renderingMode(.original)
+                }.foregroundColor(.white)
+                .padding()
+            }
+            .background(Color("Color"))
+            .cornerRadius(8)
+            
+        }
+    }
+}
+
+//struct JoinerBottomView_Previews: PreviewProvider {
+//    @Binding var showingTopPopup: Bool
+//    static var previews: some View {
+//        JoinerBottomView(viewModel: ImagesViewModel(merger: Merger(), saver: Saver()), showingTopPopup: <#Binding<Bool>#>)
+//    }
+//}
---
+++ PhotoJoiner/Views🖥/MergeViews/JoinerView.swift
@@ -0,0 +1,134 @@
+//
+//  JoinerView.swift
+//  JoinerView
+//
+//  Created by Alexander Kraev on 08.08.2020.
+//  Copyright © 2020 Alexander Kraev. All rights reserved.
+//
+
+import SwiftUI
+import SlidingRuler
+import PopupView
+
+struct JoinerView: View {
+    //for showing popup view to notify about exporting
+    @State private var showingTopPopup = false
+    
+    @ObservedObject var viewModel: ImagesViewModel
+    
+    @State private var columns: Double = 1
+    @State private var margin: Double = 0
+    @State private var showRuller = false
+
+    private let style: Style
+    
+    init(style: Style, viewModel: ImagesViewModel) {
+        self.style = style
+        self.viewModel = viewModel
+//        switch self.style{
+//            case .horizontal:
+//                columns = Double(self.viewModel.images.count)
+//            case .vertical:
+//                columns = 1
+//            case .table:
+//                self.showRuller = true
+//                columns = 2
+//        }
+    }
+    
+    var body: some View {
+        LinearGradient(gradient: .init(colors: [Color("Top"),Color("Bottom")]), startPoint: .top, endPoint: .bottom)
+            .edgesIgnoringSafeArea(.all).overlay(
+                ZStack{
+                    VStack{
+                        Image(uiImage: self.viewModel.mergedImage)
+                            .resizable()
+                            .aspectRatio(nil, contentMode: .fit)
+                            .frame(width: UIScreen.main.bounds.width - 30,
+                                   height:UIScreen.main.bounds.height / 2.5,
+                                   alignment: .center )
+                        
+//                        JoinerSettingsView(viewModel: self.viewModel,
+//                            style: self.style,
+//                            columns: Double(self.columns))
+                        
+                        GeometryReader{geo in
+                            VStack(alignment: .leading){
+                                Spacer()
+                                if showRuller {
+                                    HStack{
+                                        Image("Columns")
+                                            .renderingMode(.template)
+                                            .foregroundColor(Color("Color"))
+                                        
+                                        SlidingRuler(value: $columns,
+                                                     in: 1...20,
+                                                     step: 1,
+                                                     onEditingChanged: {dragStarts in
+                                                        if(!dragStarts){
+                                                            self.viewModel.merge(columns: Int(columns), spaceBetweenImages: margin)
+                                                        }
+                                                     } ).padding()
+                                    }.padding()
+                                }
+                                
+                                HStack{
+                                    Image("Margin")
+                                        .renderingMode(.template)
+                                        .foregroundColor(Color("Color"))
+
+                                    SlidingRuler(value: $margin,
+                                                 in: 0...1000,
+                                                 step: 20,
+                                                 onEditingChanged: {dragStarts in
+                                                    if(!dragStarts){
+                                                        self.viewModel.merge(columns: Int(columns), spaceBetweenImages: margin)
+                                                    }
+                                                 } ).padding()
+                                }
+                                .padding()
+                                Spacer()
+                            }
+                        }
+                        .clipShape(Rounded())
+                        .padding(.top, -75)
+                        
+                        JoinerBottomView(viewModel: self.viewModel,
+                                         shuffleTapped: { (finishedShuffling) in
+                                            if (finishedShuffling){
+                                                self.viewModel.merge(columns: Int(columns), spaceBetweenImages: margin)
+                                            }
+                                         },
+                                         savingTapped: {
+                                            self.showingTopPopup = true }
+                        )
+                    }
+                    .onAppear(perform: {
+                        switch self.style{
+                            case .horizontal:
+                                columns = Double(self.viewModel.images.count)
+                            case .vertical:
+                                columns = 1
+                            case .table:
+                                self.showRuller = true
+                                columns = 2
+                        }
+                        self.viewModel.merge(columns: Int(columns), spaceBetweenImages: margin)
+                    })
+                }.popup(isPresented: $showingTopPopup, type: .floater(), position: .top, animation: Animation.spring(), autohideIn: 1) {
+                    HStack {
+                        Text("Saved")
+                    }.foregroundColor(.white)
+                    .frame(width: UIScreen.main.bounds.width, height: 60)
+                    .background(Color("Color"))
+                    .cornerRadius(8.0)
+                }
+            )
+    }
+}
+
+//struct JoinerView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        JoinerView()
+//    }
+//}
